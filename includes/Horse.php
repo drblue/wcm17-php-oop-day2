68a2f9512324afea753098ba921e3891167b98e6
Johan Nordström <johan@digitalvillage.se>
2018-02-15 10:37:40 +0100

    Part 2

--- includes/Horse.php
+++ includes/Horse.php
@@ -3,5 +3,26 @@
 class Horse {
     public $name;
     public $breed;
-    public $color;
+    public $color = "White";
+    public $height = "N/A";
+    public $weight = "N/A";
+    public $sex = "Unknown";
+    public $owner;
+
+    public function addOwner($newOwner) {
+        $this->owner = $newOwner;
+    }
+
+    public function getOwner() {
+        return $this->owner;
+    }
+
+    public function getInfo() {
+        return "Name: " . $this->name . "<br>" .
+            "Breed: " . $this->breed . "<br>" .
+            "Color: " . $this->color . "<br>" .
+            "Height: " . $this->height . "<br>" .
+            "Weight: " . $this->weight . "<br>" .
+            "Sex: " . $this->sex . "<br>";
+    }
 }
--- stable.php
+++ stable.php
@@ -5,19 +5,26 @@ require "includes/Horse.php";
 $bengt = new Horse;
 $bengt->name = "Bengt";
 $bengt->breed = "Ardenner";
-$bengt->weight = 845;
+$bengt->addOwner("Eva");
 
 $kalle = new Horse;
 $kalle->name = "Kalle";
 $kalle->breed = "Arabiskt fullblod";
 $kalle->weight = 1150;
+$kalle->addOwner("Charles");
 
-$hastar = [$bengt, $kalle]; // samma som nedan, bara annat sätt att skriva på
-$hastar = array($bengt, $kalle);
+$pelle = new Horse;
+$pelle->name = "Pelle";
+$pelle->breed = "Belgisk våffla";
+$pelle->weight = 150;
+
+// $hastar = [$bengt, $kalle]; // samma som nedan, bara annat sätt att skriva på
+$hastar = array($bengt, $kalle, $pelle);
+
+// echo $hastar[1]->getInfo();
 
 foreach ($hastar as $hast) {
-    echo "<h1>" . $hast->name . "</h1>";
-    echo "Name: " . $hast->name . "<br>";
-    echo "Breed: " . $hast->breed . "<br>";
-    echo "Weight: " . $hast->weight . "<br>";
+    echo $hast->getInfo();
+    echo "Ägare: " . $hast->getOwner() . "<br>";
+    echo "<br>";
 }
